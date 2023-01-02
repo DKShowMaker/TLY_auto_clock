afb9134d525fc6badcc2781b83c8463caf566cbe
ztr <1320839695@qq.com>
2023-01-02 10:49:53 +0800

    sixth

--- main.py
+++ main.py
@@ -61,5 +61,6 @@ def login():
         button2=browser.find_element(By.XPATH,'/html/body/div/div/section[2]/div[2]/div[3]/div/div[2]/div/div[2]/div/div[2]/form/div[3]/div/input')
         print('success2')
         button2.click()
+        time.sleep(5)
         browser.quit()
 login()
