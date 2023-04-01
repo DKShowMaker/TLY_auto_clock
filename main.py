179c894e5205cee2475eafd9c92b8e64f56cd083
DKShowMaker <84145793+DKShowMaker@users.noreply.github.com>
2023-04-02 00:47:38 +0800

    使用undetected_chromedriver绕过爬虫检测

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from lxml import etree
+import undetected_chromedriver as uc
 import cv2
 
 count = 1
@@ -25,14 +26,8 @@ def login():
     tlycookie['user_email'] = '1320839695%40qq.com'
     tlycookie['is_web'] = '1'
     # 浏览器
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--disable-dev-shm-usage')
     chromedriver = "/usr/bin/chromedriver"
-    os.environ["webdriver.chrome.driver"] = chromedriver
-    browser = webdriver.Chrome(options=chrome_options, executable_path=chromedriver)
+    browser = uc.Chrome(options=chrome_options, executable_path=chromedriver)
     # 通过程序调用谷歌浏览器，chromedriver需要下载，然后下载路径填到里面。
     browser.get(url)
     for cookie in tlycookie:
